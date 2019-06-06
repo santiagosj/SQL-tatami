b6acfd77f953b3a8bc11f214ae3a64cbf768b553
Santiago Spinetto Jung <sanjs965@gmail.com>
2019-06-06 12:47:33 -0300

    queries

--- queries.sql
+++ queries.sql
@@ -252,3 +252,73 @@ EJ => Write a new query that retrieves the name, year, and imdb_rating columns o
 SELECT name, year, imdb_rating
 FROM movies
 ORDER BY imdb_rating DESC;
+/*
+Limit
+We’ve been working with a fairly small table (fewer than 250 rows), but most SQL tables contain hundreds of thousands of records. In those situations, it becomes important to cap the number of rows in the result.
+
+For instance, imagine that we just want to see a few examples of records.
+
+SELECT *
+FROM movies
+LIMIT 10;
+LIMIT is a clause that lets you specify the maximum number of rows the result set will have. This saves space on our screen and makes our queries run faster.
+
+Here, we specify that the result set can’t have more than 10 rows.
+
+LIMIT always goes at the very end of the query. Also, it is not supported in all SQL databases.
+
+1.
+write a query that returns the top 3 highest rated movies.
+
+Select all the columns.
+*/
+
+SELECT *
+FROM movies
+ORDER BY imdb_rating DESC
+LIMIT 3;
+
+/*
+Case
+A CASE statement allows us to create different outputs (usually in the SELECT statement). It is SQL’s way of handling if-then logic.
+
+Suppose we want to condense the ratings in movies to three levels:
+
+If the rating is above 8, then it is Fantastic.
+If the rating is above 6, then it is Poorly Received.
+Else, Avoid at All Costs.
+SELECT name,
+ CASE
+  WHEN imdb_rating > 8 THEN 'Fantastic'
+  WHEN imdb_rating > 6 THEN 'Poorly Received'
+  ELSE 'Avoid at All Costs'
+ END
+FROM movies;
+Each WHEN tests a condition and the following THEN gives us the string if the condition is true.
+The ELSE gives us the string if all the above conditions are false.
+The CASE statement must end with END.
+In the result, you have to scroll right because the column name is very long. To shorten it, we can rename the column to ‘Review’ using AS:
+
+SELECT name,
+ CASE
+  WHEN imdb_rating > 8 THEN 'Fantastic'
+  WHEN imdb_rating > 6 THEN 'Poorly Received'
+  ELSE 'Avoid at All Costs'
+ END AS 'Review'
+FROM movies;
+
+1.
+Select the name column and use a CASE statement to create the second column that is:
+
+‘Chill’ if genre = 'romance'
+‘Chill’ if genre = 'comedy'
+‘Intense’ in all other cases
+*/
+
+SELECT name,
+ CASE 
+  WHEN genre = 'romance' THEN  'Chill'
+  WHEN genre = 'comedy' THEN  'Chill'
+  ELSE 'Intense'
+ END AS 'Mood'
+FROM movies;
